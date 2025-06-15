df884848b40d53ac480255d735a5cc3e51fc97ad
Ivan_K <ikalinov1989yandex.ru>
2025-06-15 15:50:11 +0400

    Homework for checking

--- catalog/services.py
+++ catalog/services.py
@@ -4,23 +4,23 @@ from catalog.models import Product, Category
 from config.settings import CACHE_ENABLED
 
 
-# def get_products_by_category_from_cache():
-#     if not CACHE_ENABLED:
-#         return Product.objects.all()
-#     key = 'products_list'
-#     products = cache.get(key)
-#     if products is not None:
-#         return products
-#     products = Product.objects.all()
-#     cache.set(key, products)
-#     cache.set(key)
-
-def get_products_by_category_from_cache(user):
-    if user.groups.filter(name='moderator').exists():
+def get_products_by_category_from_cache():
+    if not CACHE_ENABLED:
         return Product.objects.all()
-    elif user.is_authenticated:
-        return Product.objects.filter(owner=user)
-    return Product.objects.none()
+    key = 'products_list'
+    products = cache.get(key)
+    if products is not None:
+        return products
+    products = Product.objects.all()
+    cache.set(key, products)
+    cache.set(key)
+
+# def get_products_by_category_from_cache(user):
+#     if user.groups.filter(name='moderator').exists():
+#         return Product.objects.all()
+#     elif user.is_authenticated:
+#         return Product.objects.filter(owner=user)
+#     return Product.objects.none()
 
 
 class CategoryService:
